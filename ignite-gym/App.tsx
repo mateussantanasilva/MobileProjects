6e72a3e0203d243fca3a2250318f23dd57b88e73
mateussantanasilva <santanasilva1778@gmail.com>
2024-03-18 22:21:09 -0300

    🎉 Fazer setup do projeto ignite-gym com o NativeBase

---
+++ ignite-gym/App.tsx
@@ -0,0 +1,26 @@
+import { Loading } from '@components/Loading'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+import { SignIn } from '@screens/SignIn'
+import { NativeBaseProvider } from 'native-base'
+import { StatusBar } from 'react-native'
+import { THEME } from 'src/theme'
+
+export default function App() {
+  const [loadedFonts] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      {loadedFonts ? <SignIn /> : <Loading />}
+    </NativeBaseProvider>
+  )
+}
